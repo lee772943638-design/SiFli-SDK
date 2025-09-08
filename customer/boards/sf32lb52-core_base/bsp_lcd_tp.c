a9223d81a35ad3c1b9ae80df936cc2a67dbbef74
minjiezhong <minjiezhong@sifli.com>
2025-09-08 16:53:17 +0800

    [bug][board] Modify the 52_core_base board.
    redmine: #7562, REDMINE-7562
    
    ext-redmine: #id, REDMINE-id
    
    [Description in detail]
    1.Added on-screen power on and off, corresponding to the switch of IO.
    2.Added the functions of exiting from and entering into sleep mode for the PSRAM.
    
    Change-Id: Iae4c1baa540578fb37dda6ef2576466f7f863d62

--- customer/boards/sf32lb52-core_base/bsp_lcd_tp.c
+++ customer/boards/sf32lb52-core_base/bsp_lcd_tp.c
@@ -23,6 +23,17 @@ void BSP_LCD_PowerDown(void)
     BSP_GPIO_Set(LCD_RESET_PIN, 0, 1);
 #ifdef LCD_USING_CO5300
     BSP_GPIO_Set(LCD_VADD_EN, 0, 1); //POwer down VADD EN
+
+    HAL_PIN_Set(PAD_PA00, GPIO_A0, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA01, GPIO_A1, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA02, GPIO_A2, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA03, GPIO_A3, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA04, GPIO_A4, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA05, GPIO_A5, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA06, GPIO_A6, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA07, GPIO_A7, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA08, GPIO_A8, PIN_PULLDOWN, 1);
+
 #endif
 }
 
@@ -32,7 +43,9 @@ void BSP_LCD_PowerUp(void)
     HAL_Delay_us(500);      // lcd power on finish ,need 500us
     BSP_PIN_LCD();
 #ifdef LCD_USING_CO5300
+    BSP_GPIO_Set(LCD_RESET_PIN, 1, 1);
     BSP_GPIO_Set(LCD_VADD_EN, 1, 1); //POwer up VADD EN
+
 #endif
 }
 
@@ -50,6 +63,11 @@ void BSP_TP_PowerDown(void)
 {
     // TODO: Setup TP power down pin
     BSP_GPIO_Set(TP_RESET,  0, 1);
+    // Touch
+    HAL_PIN_Set(PAD_PA09, GPIO_A9,  PIN_PULLDOWN, 1);    // TP_INT
+    HAL_PIN_Set(PAD_PA10, GPIO_A10, PIN_PULLDOWN, 1);    // TP_RESET
+    HAL_PIN_Set(PAD_PA20, GPIO_A20, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA11, GPIO_A11, PIN_PULLDOWN, 1);
 }
 void BSP_TP_Reset(uint8_t high1_low0)
 {
